f0d7d27e846e22c66bf8a3eed181358d3e4930fc
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-10-08 12:11:30 +0200

    Adjust tailwind config

--- src/content/components/app.tsx
+++ src/content/components/app.tsx
@@ -1,31 +1,24 @@
-import { useState } from "react";
-
 import * as Dialog from "@radix-ui/react-dialog";
 
-const App = () => {
-  const [num, setNum] = useState(1);
+const App = () => (
+  <Modal>
+    <div className="h-14 w-[43rem] rounded-md bg-white" />
+  </Modal>
+);
 
-  return (
-    <>
-      <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <button>Open</button>
-        </Dialog.Trigger>
+const Modal = ({ children }: { children: React.ReactNode }) => (
+  <Dialog.Root>
+    <Dialog.Trigger asChild>
+      <button>Open</button>
+    </Dialog.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-black/60 fixed inset-0" />
-          <Dialog.Content className="bg-white rounded-md fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[90vw] max-w-md max-h-[85vh] p-6">
-            <Dialog.Title>Some title</Dialog.Title>
-            <Dialog.Description>Some Description</Dialog.Description>
-            Other content...
-            <div>
-              {num} <button onClick={() => setNum(num + 1)}>Up</button>
-            </div>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </>
-  );
-};
+    <Dialog.Portal>
+      <Dialog.Overlay className="fixed inset-0 bg-black/60" />
+      <Dialog.Content className="fixed left-1/2 top-0 -translate-x-1/2">
+        {children}
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
 
 export default App;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+  corePlugins: {
+    preflight: false,
+  },
+} satisfies Config;
