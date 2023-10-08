34e5114cf76f6352e2699a76c6a6196d1c226dae
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-10-08 19:43:05 +0200

    Adjust styling overrides & icons

--- src/content/components/app.tsx
+++ src/content/components/app.tsx
@@ -31,13 +31,13 @@ const App = ({ unmount }: { unmount: () => void }) => {
 };
 
 const SearchBar = (props: React.ComponentProps<"input">) => (
-  <div className="mb-3 flex h-14 w-[43rem] items-center place-self-center overflow-hidden rounded-md bg-white font-semibold text-slate-500 lg:col-span-2 2xl:col-span-3">
-    <MagnifyingGlassIcon className="h-5 w-5 p-5" />
+  <div className="mb-3 flex h-14 w-[43rem] max-w-full items-center place-self-center overflow-hidden rounded-md bg-white lg:col-span-2 2xl:col-span-3">
+    <MagnifyingGlassIcon className="h-full w-14 px-5 text-slate-700" />
     <input
       {...props}
-      className="h-full flex-1 border-none font-semibold text-slate-500 focus-visible:outline-none"
+      className="h-full flex-1 border-none text-[0.94rem] font-semibold text-slate-500 "
     />
-    <Cross2Icon className="h-5 w-5 p-5" />
+    <Cross2Icon className="h-full w-14 px-5 text-slate-700" />
   </div>
 );
 
