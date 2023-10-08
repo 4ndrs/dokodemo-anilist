f93b6918aa5f12aa852309913461776c4410d44d
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-10-08 13:25:12 +0200

    Add some initial search bar styling

--- src/content/components/app.tsx
+++ src/content/components/app.tsx
@@ -1,9 +1,19 @@
 import * as Dialog from "@radix-ui/react-dialog";
 
+import { Cross2Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+
 const App = () => (
   <Modal>
-    <div className="h-14 w-[43rem] rounded-md bg-white" />
+    <SearchBar />
   </Modal>
+);
+
+const SearchBar = () => (
+  <div className="flex h-14 w-[43rem] items-center overflow-hidden rounded-md bg-white font-semibold text-slate-500">
+    <MagnifyingGlassIcon className="h-5 w-5 p-5" />
+    <input className="h-full flex-1 border-none font-semibold text-slate-500 focus-visible:outline-none" />
+    <Cross2Icon className="h-5 w-5 p-5" />
+  </div>
 );
 
 const Modal = ({ children }: { children: React.ReactNode }) => (
@@ -14,7 +24,7 @@ const Modal = ({ children }: { children: React.ReactNode }) => (
 
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 bg-black/60" />
-      <Dialog.Content className="fixed left-1/2 top-0 -translate-x-1/2">
+      <Dialog.Content className="fixed left-1/2 top-20 -translate-x-1/2">
         {children}
       </Dialog.Content>
     </Dialog.Portal>
