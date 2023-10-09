5430033159bfc3be83c339bdf53eea5ba337a680
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-10-09 23:13:40 +0200

    Use rollup as bundler
    Couldn't get production environment to work with bun's

---
+++ rollup.config.ts
@@ -0,0 +1,33 @@
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { defineConfig } from "rollup";
+
+export default defineConfig([
+  {
+    input: "src/content/dokodemo-anilist.tsx",
+    output: {
+      file: "dist/content/dokodemo-anilist.js",
+      format: "cjs",
+    },
+    plugins: [
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      typescript(),
+      nodeResolve(),
+      commonjs(),
+    ],
+  },
+  {
+    input: "src/background.ts",
+    output: {
+      file: "dist/background.js",
+      format: "cjs",
+    },
+    plugins: [typescript()],
+  },
+]);
