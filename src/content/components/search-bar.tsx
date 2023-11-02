5ee6875aa8cde85f3ed619aa89581c4d17c96d62
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-11-02 19:59:59 +0100

    Change the search bar icons' styling
    + add settings button

--- src/content/components/search-bar.tsx
+++ src/content/components/search-bar.tsx
@@ -1,22 +1,38 @@
-import { Cross2Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import {
+  Cross2Icon,
+  MagnifyingGlassIcon,
+  GearIcon,
+} from "@radix-ui/react-icons";
 
 interface Props extends React.ComponentProps<"input"> {
   isLoading: boolean;
 }
 
 const SearchBar = (props: Props) => (
-  <div className="dokodemo-mb-[12px] dokodemo-flex dokodemo-h-[56px] dokodemo-w-full dokodemo-max-w-[688px] dokodemo-items-center  dokodemo-place-self-center dokodemo-overflow-hidden dokodemo-rounded-[6px] dokodemo-bg-white lg:dokodemo-col-span-2 2xl:dokodemo-col-span-3">
-    <MagnifyingGlassIcon className="dokodemo-h-full dokodemo-w-[56px] dokodemo-px-[20px] dokodemo-text-slate-700" />
+  <div className="dokodemo-mb-[12px] dokodemo-flex dokodemo-h-[56px] dokodemo-w-full dokodemo-max-w-[688px] dokodemo-items-center dokodemo-place-self-center  dokodemo-overflow-hidden dokodemo-rounded-[6px] dokodemo-bg-white dokodemo-px-4 lg:dokodemo-col-span-2 2xl:dokodemo-col-span-3">
+    <MagnifyingGlassIcon className="dokodemo-mx-1 dokodemo-h-[20px] dokodemo-w-[20px] dokodemo-text-slate-700" />
     <input
       {...props}
-      className="dokodemo-h-full dokodemo-flex-1 dokodemo-border-none dokodemo-bg-white dokodemo-text-[15px] dokodemo-font-semibold dokodemo-capitalize dokodemo-text-slate-500 dokodemo-placeholder-slate-400"
+      className="dokodemo-mx-2 dokodemo-h-full dokodemo-flex-1 dokodemo-border-none dokodemo-bg-white dokodemo-text-[15px] dokodemo-font-semibold dokodemo-capitalize dokodemo-text-slate-500 dokodemo-placeholder-slate-400"
     />
     <div
       className={`${
         props.isLoading ? "dokodemo-block" : "dokodemo-hidden"
-      } dokodemo-h-[25px] dokodemo-w-[25px] dokodemo-animate-spin dokodemo-rounded-full dokodemo-border-[3px] dokodemo-border-solid dokodemo-border-blue-300 dokodemo-border-t-transparent`}
+      } dokodemo-mx-1 dokodemo-h-[20px] dokodemo-w-[20px] dokodemo-animate-spin dokodemo-rounded-full dokodemo-border-[3px] dokodemo-border-solid dokodemo-border-blue-300 dokodemo-border-t-transparent`}
     />
-    <Cross2Icon className="dokodemo-h-full dokodemo-w-[56px] dokodemo-px-[20px] dokodemo-text-slate-700" />
+    <button
+      aria-label="Settings"
+      onClick={() => console.log("goooooooooooooooooood morning miyakejimaaaa")}
+      className="dokodemo-group dokodemo-mx-1 dokodemo-flex dokodemo-cursor-pointer dokodemo-rounded-full dokodemo-border-none dokodemo-bg-transparent"
+    >
+      <GearIcon className="dokodemo-h-[20px] dokodemo-w-[20px] dokodemo-text-slate-700 dokodemo-transition-colors dokodemo-duration-300 group-hover:dokodemo-text-sky-600 group-focus-visible:dokodemo-text-sky-600" />
+    </button>
+    <button
+      aria-label="Close"
+      className="dokodemo-group dokodemo-mx-1 dokodemo-flex dokodemo-cursor-pointer dokodemo-rounded-full dokodemo-border-none dokodemo-bg-transparent"
+    >
+      <Cross2Icon className="dokodemo-h-[20px] dokodemo-w-[20px] dokodemo-text-slate-700 dokodemo-transition-colors dokodemo-duration-300 group-hover:dokodemo-text-sky-600 group-focus-visible:dokodemo-text-sky-600" />
+    </button>
   </div>
 );
 
